48369715f87d3781408f19cde948417f27812e9f
riskmaris <mariskaps20@gmail.com>
2023-08-08 20:21:01 +0300

    add events file

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,40 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Result from './result';
+import calculate from '../logic/calculate';
 
 function Calculator() {
+  const [state, setState] = useState({ total: 0 });
+  const btnClickHandler = (e) => {
+    const math = calculate(state, e.target.innerHTML);
+    setState(math);
+  };
+
   return (
     <div className="calulator-container">
       <Result />
       <div className="wrapper-operation">
         <div>
-          <button type="button" className="other-operations btn">AC</button>
-          <button type="button" className="other-operations btn">+/-</button>
-          <button type="button" className="other-operations btn">%</button>
-          <button type="button" className="other-operations btn operator">&#247;</button>
+          <button onClick={btnClickHandler} type="button" className="other-operations btn">AC</button>
+          <button onClick={btnClickHandler} type="button" className="other-operations btn">+/-</button>
+          <button onClick={btnClickHandler} type="button" className="other-operations btn">%</button>
+          <button onClick={btnClickHandler} type="button" className="other-operations btn operator">&#247;</button>
         </div>
 
         <div className="Digits-container">
-          <button type="button" className="digits-btn btn">7</button>
-          <button type="button" className="digits-btn btn">8</button>
-          <button type="button" className="digits-btn btn">9</button>
-          <button type="button" className="digits-btn  operator">X</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">7</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">8</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">9</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn  operator">X</button>
         </div>
         <div className="Digits-container">
-          <button type="button" className="digits-btn btn">4</button>
-          <button type="button" className="digits-btn btn">5</button>
-          <button type="button" className="digits-btn btn">6</button>
-          <button type="button" className="digits-btn  operator">-</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">4</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">5</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">6</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn  operator">-</button>
         </div>
         <div className="Digits-container">
-          <button type="button" className="digits-btn btn">1</button>
-          <button type="button" className="digits-btn btn">2</button>
-          <button type="button" className="digits-btn btn">3</button>
-          <button type="button" className="digits-btn  operator">+</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">1</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">2</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn btn">3</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn  operator">+</button>
         </div>
         <div className="Digits-container">
-          <button type="button" className="last-btn btn">0</button>
-          <button type="button" className="digits-btn  btn">.</button>
-          <button type="button" className="digits-btn operator">=</button>
+          <button onClick={btnClickHandler} type="button" className="last-btn btn">0</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn  btn">.</button>
+          <button onClick={btnClickHandler} type="button" className="digits-btn operator">=</button>
         </div>
       </div>
     </div>
--- src/components/result.js
+++ src/components/result.js
@@ -2,6 +2,9 @@ function result() {
   return (
     <div>
       <div className="input-filed">
+        {State.total}
+        {State.operation}
+        {State.next}
         0
       </div>
     </div>
