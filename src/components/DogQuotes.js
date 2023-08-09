fea42084fb2dd8f29b839da53ddad350c777a4b6
riskmaris <mariskaps20@gmail.com>
2023-08-09 15:12:46 +0300

    fix linter error

--- src/components/DogQuotes.js
+++ src/components/DogQuotes.js
@@ -1,55 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-
-// const Quotes = () => {
-//   const [quote, setQuote] = useState('');
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=dogs', {
-//           headers: {
-//             'X-Api-Key': '0oWa33wHCcpfBcURu9UPv39A2rSCT1DcyejUtSf7',
-//             'Content-Type': 'application/json',
-//           },
-//         });
-
-//         if (response.ok) {
-//           const data = await response.json();
-//           const randomIndex = Math.floor(Math.random() * data.length);
-//           setIsLoading(false);
-//           setQuote(data[randomIndex]?.quote);
-//         } else {
-//           setError('Error fetching quotes');
-//           setIsLoading(false);
-//         }
-//       } catch {
-//         setError('Error fetching quotes');
-//         setIsLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className="quotes">
-//       {isLoading && <p className="randomQuotes">Loading...</p>}
-//       {!isLoading && error && (
-//       <p className="randomQuotes">
-//         Error:
-//         {error}
-//       </p>
-//       )}
-//       {!isLoading && !error && <p className="randomQuotes">{quote}</p>}
-//     </div>
-//   );
-// };
-
-// export default Quotes;
-
-
 import React, { useState, useEffect } from 'react';
 
 function DogQuotes() {
@@ -70,10 +18,10 @@ function DogQuotes() {
         setQuote(data[0]?.quote || 'No quotes available.');
         setLoading(false);
       })
-      .catch((err) => {
-        setError('An error occurred while fetching quotes.');
-        setLoading(false);
-      });
+      // .catch((err) => {
+      //   setError('An error occurred while fetching quotes.');
+      //   setLoading(false);
+      // });
   }, []);
 
   if (loading) {
