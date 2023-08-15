5994b3e27908ef46593d36a9c023c48af02cee7f
riskmaris <mariskaps20@gmail.com>
2023-08-15 15:24:47 +0300

    create Math-Magician full website

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
+// import React from 'react';
+// import './index.css';
+// import Calculator from './components/calculator';
+// import DatingQuotes from './components/DatingQuotes';
+
+// function App() {
+//   return (
+//     <div>
+//       <Calculator />
+//       <DatingQuotes />
+//     </div>
+
+//   );
+// }
+
+// export default App;
+
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Quote from './components/Quote';
 import './index.css';
+import './App.css';
 import Calculator from './components/calculator';
-import DatingQuotes from './components/DatingQuotes';
 
 function App() {
   return (
-    <div>
-      <Calculator />
-      <DatingQuotes />
+    <div className="App">
+
+      <Router>
+        <Nav />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </Router>
     </div>
 
   );
---
+++ src/components/Quote.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>
+      <h1> Math Magiciancian</h1>
+      <p className="quote">
+        Mathmatics is not about numbers ,equations computation, or algorithm
+        its about understanding. - William paul Thurston
+      </p>
+    </div>
+
+  );
+}
+
+export default Quote;
